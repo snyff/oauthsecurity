92f1b2cddc6f9b233b81a79e2ceb0e8c6010ce3e
Egor Homakov <homakov@homakov.local>
2014-05-12 20:04:45 +0700

    1

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'http://rubygems.org'
 gem 'sinatra'
 gem 'slim'
+gem 'rdiscount'
+gem 'haml'
--- app.rb
+++ app.rb
@@ -1,6 +1,25 @@
 require 'rubygems'
 require 'sinatra'
+require 'rdiscount'
+require 'haml'
+
+
+
+set :views, settings.root
+set :public_folder, 'dist'
+
+#set :show_exceptions, false
+
+disable :sessions
+disable :protection
+
 
 get '/' do
+  # use index.haml for readme
+  File.open('dist/index.html')
+  #markdown :a #, :layout => :index
+end
+
+get '/d' do
   File.open('a.html')
 end
